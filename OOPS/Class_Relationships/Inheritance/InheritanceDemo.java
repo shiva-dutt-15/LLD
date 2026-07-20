1075cc3beab9a8ec42e1b8f081c057d3a107ea07
shiva-dutt-15 <shivadutt.vishnubhatta@gmail.com>
2026-07-20 09:52:07 +0530

    Class relationship - Inheritance demo

---
+++ OOPS/Class_Relationships/Inheritance/InheritanceDemo.java
@@ -0,0 +1,9 @@
+// Defines 'is-a' relationship between Dog and Animal
+
+class InheritanceDemo{
+    public static void main(String[] args){
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
