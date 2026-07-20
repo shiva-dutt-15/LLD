4837ec8479c0450c52c25927a8705a5da46dbef9
shiva-dutt-15 <shivadutt.vishnubhatta@gmail.com>
2026-07-20 09:40:55 +0530

    Class and Object test

---
+++ OOPS/Classes/car.java
@@ -0,0 +1,21 @@
+public class Car{
+
+    String name;
+    String manufacturer;
+    int year;
+
+    public Car(String name, String manufacturer, int year){
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.year = year
+    }
+
+    public void startEngine(){
+        System.out.println("The "+ name + manufacturer + " of " + year +"'s is starting");
+    }
+
+    public void displayInfo(){
+        System.out.println("Car info: " + name + " " + manufacturer + "(" + year + ")");
+    }
+
+}
